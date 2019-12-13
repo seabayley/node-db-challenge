26e277c139ec520c0f780f6b1424b31c7ae1fc32
Christopher Bailey <seabayley@outlook.com>
2019-12-13 10:17:46 -0600

    Initial setup and migration

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/recipes.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+
+}
